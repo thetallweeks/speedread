a2cbfdf694f70823ea4d1e70bf6f4516df56f0e9
kmweeks <kevinmichaelweeks@gmail.com>
2013-11-07 07:35:22 -0800

    Added functionality to display words on a custom interval

--- js/main.js
+++ js/main.js
@@ -3,20 +3,54 @@
   // Set #spreedyTextInput as a variable for reuse
   var spreedyTextInput = $('#spreedyTextInput');
 
-  function wordCount(inputText) {
-    inputText.on('keyup propertychange paste', function() {
-      var words = spreedyTextInput.val().split(' ').length;
+  var spreedyButtonStart = $('#spreedyButtonStart');
 
-      // Start word count at 0
-      if(spreedyTextInput.val().length === 0){
-        words = 0;
-      }
+  // Set words in the global scope
+  var words = [];
 
-      // Update word count
-      $('#spreedyWordCount').html(words + ' words');
-    });
+  // Watch textarea for changes
+  spreedyTextInput.on('keyup propertychange paste', function() {
+    splitIntoWords(spreedyTextInput);
+    wordCounter(words);
+  });
+
+  spreedyButtonStart.click(function() {
+    displayWords(words);
+  });
+
+  // Creates words array from input text
+  function splitIntoWords(inputText) {
+    words = spreedyTextInput.val().trim().split(' ');
   }
 
-  wordCount(spreedyTextInput);
+  // Counts items in the words array and updates word count
+  function wordCounter(wordsArray) {
+      // Start word count at 0
+      var wordCount = wordsArray.length;
+
+      // Update word count
+      $('#spreedyWordCount').html(wordCount + ' words');
+  }
+
+  // Displays words at custom interval
+  function displayWords(wordsArray, speed) {
+    speed = speed || 300;
+    var i = 0;
+
+    var displayWordsInterval = setInterval(function() {
+
+      // Replace html with subsequent word in the array
+      document.getElementById('spreedyWordDisplay').innerHTML = wordsArray[i++];
+
+      // Stop SetInterval when counter = array length
+      if(i === wordsArray.length) {
+
+        // SetIntervalID is the function name
+        clearInterval(displayWordsInterval);
+      }
+
+    }, speed);
+  }
+
 
 })(jQuery);
