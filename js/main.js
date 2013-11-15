c109cd2635d52f2fa3e3c0d037ff86298efda850
kmweeks <kevinmichaelweeks@gmail.com>
2013-11-15 01:06:29 -0800

    Styled UI, updated javascript with additional functionality

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ var SPREEDY = {
 
   config : {
 
-    // Sets default words per minute (in miliseconds)
-    speed : 200
+    // Sets default words per minute
+    speed : 300
 
   },
 
@@ -17,27 +17,65 @@ var SPREEDY = {
     SPREEDY.wordCount = $('#spreedyWordCount');
     SPREEDY.playPauseButton = $('#spreedyPlayPause');
     SPREEDY.stopButton = $('#spreedyStop');
+    SPREEDY.popupMenus = $('.app-controls-menu');
+
+    // Speed Settings
+    SPREEDY.speedSettings = $('#spreedySpeedSettings');
+    SPREEDY.speedSettingsMenu = $('#spreedySpeedSettingsMenu');
+    SPREEDY.speedInput = $('#spreedySpeedInput');
+    SPREEDY.speed = $('#spreedySpeed');
 
     // Set the word display to hidden by default
     SPREEDY.wordDisplayContainer.addClass('is-hidden');
+
+    // Hide Speed Settings popup
+    SPREEDY.popupMenus.addClass('is-hidden');
+
+    // Set new Speed value
+    SPREEDY.speedInput.on('keyup propertychange paste', function() {
+      SPREEDY.setSpeed();
+    });
+
+    // Hide menus when clicking off of target
+    $('html').click(function() {
+      SPREEDY.popupMenus.addClass('is-hidden');
+      SPREEDY.speedSettings.removeClass('is-active');
+    });
 
     // Begin watching text input
     SPREEDY.watch(SPREEDY.textInput);
 
     var displayWordsInterval;
 
-    // Bind click functions
+    // Bind click function for play/pause
     SPREEDY.playPauseButton.click(function() {
-        SPREEDY.playPauseButton.removeClass('icon-play').addClass('icon-pause');
-        SPREEDY.displayWords();
+      // TODO: Check for empty textarea
+      SPREEDY.playPauseButton.removeClass('icon-play').addClass('icon-pause');
+      SPREEDY.displayWords(SPREEDY.config.speed);
 
-        // TODO: Set up state for play/pause
+      // TODO: Set up state for play/pause
     });
 
+    // Bind click function for Stop
     SPREEDY.stopButton.click(function() {
       SPREEDY.stopDisplayWords();
     });
 
+    // Bind click for Speed Settings
+    SPREEDY.speedSettings.click(function() {
+      event.stopPropagation();
+      if(SPREEDY.speedSettingsMenu.hasClass('is-hidden')) {
+        $(this).addClass('is-active');
+        SPREEDY.speedSettingsMenu.removeClass('is-hidden');
+      } else {
+        $(this).removeClass('is-active');
+        SPREEDY.speedSettingsMenu.addClass('is-hidden');
+      }
+    });
+
+    SPREEDY.speedSettingsMenu.click(function() {
+      event.stopPropagation();
+    });
   },
 
   watch : function(input) {
@@ -63,11 +101,20 @@ var SPREEDY = {
     var wordCount = array.length;
 
     // Update word count with new value
-    SPREEDY.wordCount.html(wordCount + ' words');
+    SPREEDY.wordCount.html(wordCount);
 
   },
 
-  displayWords : function() {
+  setSpeed : function() {
+
+    SPREEDY.config.speed = SPREEDY.speedInput.val();
+
+    // Update Speed display
+    SPREEDY.speed.html(SPREEDY.config.speed);
+
+  },
+
+  displayWords : function(speed) {
 
     // Show Word Display
     SPREEDY.wordDisplayContainer.removeClass('is-hidden');
@@ -75,13 +122,14 @@ var SPREEDY = {
     // Hide Input
     SPREEDY.textInputContainer.addClass('is-hidden');
 
+    speed = (1 / (speed / 60)) * 1000;
+
     var i = 0;
 
+    // TODOS: Speed causes wait before first word and
+    // pace seems inconsistent
     displayWordsInterval = setInterval(function() {
 
-      // Replace html with subsequent word in the array
-      // This can be done without jQuery using:
-      // document.getElementById('spreedyWordDisplay').innerHTML = wordsArray[i++];
       SPREEDY.wordDisplay.html(words[i++]);
 
       // Stop SetInterval when counter = array length
@@ -89,9 +137,11 @@ var SPREEDY = {
 
         // SetIntervalID is the function name
         clearInterval(displayWordsInterval);
+
+        SPREEDY.playPauseButton.removeClass('icon-pause').addClass('icon-play');
       }
 
-    }, SPREEDY.config.speed);
+    }, speed);
 
   },
 
