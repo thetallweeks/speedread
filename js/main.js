3057fe6fe660a17a226fe7b5ac84d405a67ef001
kmweeks <kevinmichaelweeks@gmail.com>
2013-11-18 23:19:41 -0800

    Updated icons, added alternate font options, styled font popup menu, set up choosing a specific font, refactored javascript

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ var SPREEDY = {
 
   config : {
 
+    // Sets default font-family
+    font: "lora",
+
     // Sets default words per minute
     speed : 300
 
@@ -9,6 +12,26 @@ var SPREEDY = {
 
   init : function(config) {
 
+    SPREEDY.createUIVariables();
+
+    SPREEDY.bindUI();
+
+    // Begin watching Speed Setting Input
+    SPREEDY.watchSpeedSetting(SPREEDY.speedInput);
+
+    // Begin watching text input
+    SPREEDY.watchTextArea(SPREEDY.textInput);
+
+    // Create interval variable in the global scope
+    // Needed for clearInterval to work
+    var displayWordsInterval;
+
+    // Set the default font
+    SPREEDY.chooseFont();
+
+  },
+
+  createUIVariables : function() {
     // Set DOM elements as variables for reuse
     SPREEDY.textInput = $('#spreedyTextInput');
     SPREEDY.textInputContainer = $('#spreedyTextInputContainer');
@@ -25,27 +48,26 @@ var SPREEDY = {
     SPREEDY.speedInput = $('#spreedySpeedInput');
     SPREEDY.speed = $('#spreedySpeed');
 
-    // Set the word display to hidden by default
-    SPREEDY.wordDisplayContainer.addClass('is-hidden');
+    // Font Settings
+    SPREEDY.fontSettings = $('#spreedyFontSettings');
+    SPREEDY.fontSettingsMenu = $('#spreedyFontSettingsMenu');
 
-    // Hide Speed Settings popup
-    SPREEDY.popupMenus.addClass('is-hidden');
+  },
 
-    // Set new Speed value
-    SPREEDY.speedInput.on('keyup propertychange paste', function() {
-      SPREEDY.setSpeed();
-    });
+  bindUI : function() {
 
     // Hide menus when clicking off of target
     $('html').click(function() {
       SPREEDY.popupMenus.addClass('is-hidden');
       SPREEDY.speedSettings.removeClass('is-active');
+      SPREEDY.fontSettings.removeClass('is-active');
     });
 
-    // Begin watching text input
-    SPREEDY.watch(SPREEDY.textInput);
+    // Set the word display to hidden by default
+    SPREEDY.wordDisplayContainer.addClass('is-hidden');
 
-    var displayWordsInterval;
+    // Hide Speed Settings popup
+    SPREEDY.popupMenus.addClass('is-hidden');
 
     // Bind click function for play/pause
     SPREEDY.playPauseButton.click(function() {
@@ -61,9 +83,16 @@ var SPREEDY = {
       SPREEDY.stopDisplayWords();
     });
 
+    // TODO: Abstract show popup to work for both
+    // Speed Settings and Font Settings
+
     // Bind click for Speed Settings
     SPREEDY.speedSettings.click(function() {
       event.stopPropagation();
+
+      // Toggle off Font Settings popup
+      SPREEDY.fontSettings.removeClass('is-active');
+      SPREEDY.fontSettingsMenu.addClass('is-hidden');
       if(SPREEDY.speedSettingsMenu.hasClass('is-hidden')) {
         $(this).addClass('is-active');
         SPREEDY.speedSettingsMenu.removeClass('is-hidden');
@@ -76,9 +105,29 @@ var SPREEDY = {
     SPREEDY.speedSettingsMenu.click(function() {
       event.stopPropagation();
     });
+
+    // Bind click for Font Settings
+    SPREEDY.fontSettings.click(function() {
+      event.stopPropagation();
+      SPREEDY.speedSettings.removeClass('is-active');
+      SPREEDY.speedSettingsMenu.addClass('is-hidden');
+      if(SPREEDY.fontSettingsMenu.hasClass('is-hidden')) {
+        $(this).addClass('is-active');
+        SPREEDY.fontSettingsMenu.removeClass('is-hidden');
+      } else {
+        $(this).removeClass('is-active');
+        SPREEDY.fontSettingsMenu.addClass('is-hidden');
+        SPREEDY.fontSettingsMenu.addClass('is-hidden');
+      }
+    });
+
+    SPREEDY.fontSettingsMenu.click(function() {
+      event.stopPropagation();
+    });
+
   },
 
-  watch : function(input) {
+  watchTextArea : function(input) {
 
     // Watch textarea for changes
     input.on('keyup propertychange paste', function() {
@@ -105,6 +154,14 @@ var SPREEDY = {
 
   },
 
+  watchSpeedSetting : function(input) {
+
+    // Set new Speed value
+    input.on('keyup propertychange paste', function() {
+      SPREEDY.setSpeed();
+    });
+  },
+
   setSpeed : function() {
 
     SPREEDY.config.speed = SPREEDY.speedInput.val();
@@ -112,6 +169,47 @@ var SPREEDY = {
     // Update Speed display
     SPREEDY.speed.html(SPREEDY.config.speed);
 
+  },
+
+  chooseFont : function() {
+    SPREEDY.setFont();
+    SPREEDY.fontSettingsMenu.find('.font-option').click(function() {
+      SPREEDY.fontSettingsMenu.find('.font-option').removeClass('is-active');
+
+      // Don't create a jQuery object here
+      SPREEDY.config.font = event.target.id;
+
+      // Use the jQuery object form of event.target to addClass
+      $(event.target).addClass('is-active');
+      SPREEDY.removeFontClass();
+      SPREEDY.setFont();
+    });
+  },
+
+  removeFontClass : function () {
+
+    // TODO: Refactor this to work more efficiently
+    // Can this be set up to work without needing to know
+    // the class that shoud be removed?
+    if(SPREEDY.wordDisplay.hasClass('lora')) {
+      SPREEDY.wordDisplay.removeClass('lora');
+    } else if(SPREEDY.wordDisplay.hasClass('montserrat')) {
+      SPREEDY.wordDisplay.removeClass('montserrat');
+    } else if(SPREEDY.wordDisplay.hasClass('sanchez')) {
+      SPREEDY.wordDisplay.removeClass('sanchez');
+    } else if(SPREEDY.wordDisplay.hasClass('poly')) {
+      SPREEDY.wordDisplay.removeClass('poly');
+    } else if(SPREEDY.wordDisplay.hasClass('pt-serif')) {
+      SPREEDY.wordDisplay.removeClass('pt-serif');
+    } else if(SPREEDY.wordDisplay.hasClass('roboto')) {
+      SPREEDY.wordDisplay.removeClass('roboto');
+    } else if(SPREEDY.wordDisplay.hasClass('georgia')) {
+      SPREEDY.wordDisplay.removeClass('georgia');
+    }
+  },
+
+  setFont : function() {
+    SPREEDY.wordDisplay.addClass(SPREEDY.config.font);
   },
 
   displayWords : function(speed) {
